7054fc437ce6c7b1dfa77b47905d0932b53af5a7
Prayag Gediya <prayag.work60@gmail.com>
2023-07-08 10:27:46 +0530

    Initial Commit

---
+++ Camera-Demo/Camera_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Camera_DemoApp.swift
+//  Camera-Demo
+//
+//  Created by Prayag Gediya on 08/07/23.
+//
+
+import SwiftUI
+
+@main
+struct Camera_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
